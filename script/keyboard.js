3e999c9a303af27c9b6f28ef2a8f99e46e733fc9
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-15 16:48:09 -0800

    Refactor to use name of key pressed instead of numeric code

--- script/keyboard.js
+++ script/keyboard.js
@@ -2,11 +2,9 @@
 document.onkeydown = useKeyboard;
 
 function useKeyboard(e) {
-  const keyPressed = (window.event || e).keyCode;
+  const keyName = (window.event || e).key.toLowerCase();
 
-  // Enter:
-
-  if (keyPressed == 13) { // enter
+  if (keyName == 'enter') {
     let focusElement = document.activeElement.id;
 
     if (focusElement == 'inputBoardSizeX' || focusElement == 'inputBoardSizeY') {
@@ -17,59 +15,53 @@ function useKeyboard(e) {
     return;
   }
 
-  // Press one of the the three buttons:
-
-  if (keyPressed == 78) { // "n"
+  if (keyName == 'n') {
     newGame();
     return;
   }
 
-  if (keyPressed == 83) { // "s"
+  if (keyName == 's') {
     shuffleBoard();
     return;
   }
 
-  if (keyPressed == 70) { // "f"
+  if (keyName == 'f') {
     solveBoard();
     return;
   }
 
-  // Focus one of the two input fields:
-
-  if (keyPressed == 72) { // "h"
+  if (keyName == 'h') {
     inputBoardSizeX.focus();
     return;
   }
 
-  if (keyPressed == 87) { // "w"
+  if (keyName == 'w') {
     inputBoardSizeY.focus();
     return;
   }
 
-  // Arrow keys:
-
-  if (keyPressed == 37) { // left
+  if (keyName == 'arrowleft') { // left
     if (emptySpace.y < boardSize.y) {
       swapTiles(emptySpace.x, emptySpace.y + 1)
     }
     return;
   }
 
-  if (keyPressed == 38) { // up
+  if (keyName == 'arrowup') { // up
     if (emptySpace.x < boardSize.x) {
       swapTiles(emptySpace.x + 1, emptySpace.y)
     }
     return;
   }
 
-  if (keyPressed == 39) { // right
+  if (keyName == 'arrowright') { // right
     if (emptySpace.y > 1) {
       swapTiles(emptySpace.x , emptySpace.y - 1)
     }
     return;
   }
 
-  if (keyPressed == 40) { // down
+  if (keyName == 'arrowdown') { // down
     if (emptySpace.x > 1) {
       swapTiles(emptySpace.x - 1, emptySpace.y)
     }
